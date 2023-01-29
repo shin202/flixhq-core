64dc1be4fafe20a2e4e80c5f977d75679409f0d1
Tran Viet Hoang <tranviethoang642@gmail.com>
2023-01-29 21:41:46 +0700

    chore: test for movie information

--- test/index.ts
+++ test/index.ts
@@ -2,6 +2,6 @@ import { MOVIES } from "../src";
 
 (async () => {
     const FlixHQ = new MOVIES.FlixHQ();
-    const data = await FlixHQ.home();
+    const data = await FlixHQ.fetchMovieInfo('tv/watch-junji-ito-maniac-japanese-tales-of-the-macabre-92398');
     console.log(data);
 })();
